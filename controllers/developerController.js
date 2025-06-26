16c4ad542f3f8ed685de2931a62ff36f9f859234
Michael <michaelbrandmeyer99@gmail.com>
2025-06-25 19:27:21 -0700

    controller

---
+++ controllers/developerController.js
@@ -0,0 +1,14 @@
+const { body, validationResult } = require("express-validator");
+const db = require("../db/queries")
+
+
+exports.getDevelopers = async (req,res) => {
+  
+  
+};
+
+exports.getDeveloper = async (req,res) => {
+  
+
+};
+
