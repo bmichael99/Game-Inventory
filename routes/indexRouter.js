32e9b2f904d4fa7d5d236afd42e3da34f5d5784c
Michael <michaelbrandmeyer99@gmail.com>
2025-06-25 19:26:40 -0700

    updated homepage routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,12 @@ const indexController = require("../controllers/indexController");
 const indexRouter = Router();
 
 
-indexRouter.get("/", indexController.showHomePage);
+//indexRouter.get("/", indexController.showHomePage);
+indexRouter.get("/", indexController.getUsernames);
+indexRouter.get("/new", indexController.usersCreateGet);
+indexRouter.post("/new", indexController.usersCreatePost);
+
+
 
 module.exports = indexRouter;
 
